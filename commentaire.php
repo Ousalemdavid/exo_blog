2c25fb7f1ebe08be9b736e1bca082d3970cbb485
Ousalemdavid <Ousalem.david@netc.fr>
2017-10-06 09:16:42 +0200

    Update commentaire.php

--- commentaire.php
+++ commentaire.php
@@ -3,7 +3,7 @@
         <?php
     try
             {
-              $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root','22Misterhoo');
+              $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root','');
             }
             catch (Exception $e)
             {
