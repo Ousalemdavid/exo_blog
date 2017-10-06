df0422975ab07c54ea6ba8efc4f63e65864b5854
Ousalemdavid <Ousalem.david@netc.fr>
2017-10-06 09:16:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
         <?php
     try
             {
-              $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '22Misterhoo');
+              $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
             }
             catch (Exception $e)
             {
